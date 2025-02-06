1391fd0f6cec9fe6052bddbe89fa8fa0aab5a34b
Yevgeniy <66377737+Yevgeniy-Olexandrenko@users.noreply.github.com>
2025-02-07 01:22:58 +0200

    Added sync local time with browser time

--- firmware/src/services/settings_webapp/SettingsWebApp.cpp
+++ firmware/src/services/settings_webapp/SettingsWebApp.cpp
@@ -1,3 +1,4 @@
+#include <GyverNTP.h>
 #include "SettingsWebApp.h"
 #include "drivers/onboard/SelfReboot.h"
 #include "defines.h"
@@ -10,6 +11,7 @@ namespace service
         settings::sets().onBuild([&](sets::Builder& b) { this->settingsBuild(b); });
         settings::sets().onUpdate([&](sets::Updater& u) { this->settingsUpdate(u); });
         settings::sets().onFocusChange([&]() { this->onFocusChange(settings::sets().focused()); });
+        settings::sets().rtc.onSync([&](uint32_t unix) { NTP.sync(unix); });
 
         settings::sets().config.theme = sets::Colors::Aqua;
         settings::sets().config.updateTout = 1000;
