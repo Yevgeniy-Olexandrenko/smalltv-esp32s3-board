200aab44423c35218b64aa4633a66320f92bccba
Yevgeniy <66377737+Yevgeniy-Olexandrenko@users.noreply.github.com>
2025-03-15 03:03:37 +0200

    Better config for TFT_eSPI library

--- firmware/include/tftespi.h
+++ firmware/include/tftespi.h
@@ -1,17 +1,24 @@
 #pragma once
 
+#define DISABLE_ALL_LIBRARY_WARNINGS
 #define USER_SETUP_LOADED
 #define ST7789_2_DRIVER
 #define LOAD_GLCD
 
-#define TFT_RGB_ORDER   TFT_RGB
-#define TFT_WIDTH       240
-#define TFT_HEIGHT      240
-#define TFT_MOSI        PIN_LCD_SDA
-#define TFT_SCLK        PIN_LCD_SCL
-#define TFT_CS          PIN_LCD_CS
-#define TFT_DC          PIN_LCD_DC
-#define TFT_RST         PIN_LCD_RES
-#define TFT_BL          GPIO_NUM_NC
-#define TOUCH_CS        GPIO_NUM_NC
-#define SPI_FREQUENCY   80000000
+#define TFT_RGB_ORDER TFT_RGB
+#define TFT_WIDTH     240
+#define TFT_HEIGHT    240
+#define SPI_FREQUENCY 80000000
+
+#undef TFT_BL
+#undef TFT_CS
+#undef TOUCH_CS
+
+#define TFT_DC    PIN_LCD_DC
+#define TFT_RST   PIN_LCD_RES
+#define TFT_MOSI  PIN_LCD_SDA
+#define TFT_SCLK  PIN_LCD_SCL
+
+#if PIN_LCD_CS != GPIO_NUM_NC
+#define TFT_CS    PIN_LCD_CS
+#endif
